7fc27bef5065ab04c97ece295f8cc7803b05fcd7
Jothish JJ <85335732+JothishJJ@users.noreply.github.com>
2023-06-08 13:44:48 +0000

    Add fontawesome

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 
-export default function Navbar() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+
+export default function Navbar() { 
     return (
       <nav 
-        className="flex bg-rose-500 py-4 text-xl justify-around"
+        className="flex bg-rose-500 py-4 text-xl justify-around items-center"
       >
          <Link href="/" className="font-main">JJChat</Link>
          <Link href="/">Home</Link>
          <Link href="/Login">Login</Link>
-         <Link href="/Sign-up">Signup</Link>
+         <Link href="/Signup">Signup</Link>
+         <FontAwesomeIcon icon={faLightbulb} />
       </nav>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import './globals.css';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 import { Inter } from 'next/font/google';
 import Navbar from './components/Navbar';
 
