bd06717049abe57db84f5db7822d24a6e47fd7fa
Jothish JJ <85335732+JothishJJ@users.noreply.github.com>
2023-06-19 14:38:07 +0000

    Fix the key

--- app/App/page.tsx
+++ app/App/page.tsx
@@ -13,9 +13,9 @@ export default function ChatApp() {
         <div className="pt-32 flex flex-col items-center px-4 gap-4">
           <h1 className="font-main text-2xl text-rose-600">Chats</h1>
           {
-            chats.map(chat => {
+            chats.map((chat, index) => {
               return (
-              <div className="w-full">
+              <div className="w-full" key={index}>
                 <div className="flex items-center gap-2 pb-2">
                   <Image src={chat.photoUrl} alt="Profile" height={50} width={50} className="rounded-3xl" />
                   <p className="font-main dark:text-white">{chat.title}</p>
