8a94c34bde5573f81d071dca889fdbd81840a19f
Jothish JJ <85335732+JothishJJ@users.noreply.github.com>
2023-06-28 06:47:34 +0000

    Add dynamic routes

---
+++ app/App/[chats]/page.tsx
@@ -0,0 +1,23 @@
+import { firestore } from "../../lib/firebase"
+import { collection, getDocs } from "firebase/firestore"
+
+export const dynamicParams = false;
+
+export default function Chat({params}) {
+    return (
+      <div className="pt-28 p-4">
+         <h1 className="dark:text-white font-main text-2xl">
+           {params.chats}
+         </h1>
+      </div>
+    )
+}
+
+export async function generateStaticParams() {
+  const paths: { chats: string; }[] = []
+  const querySnapshot = await getDocs(collection(firestore, `chats`));
+  querySnapshot.forEach((doc) => {
+    paths.push({chats: doc.id});
+  })
+  return paths;
+}
