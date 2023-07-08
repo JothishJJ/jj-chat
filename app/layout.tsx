8e3c76a4a7a184425a5e1aa80d477b884437e617
Jothish JJ <85335732+JothishJJ@users.noreply.github.com>
2023-07-08 17:14:38 +0000

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,26 @@ import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const title = "JJ Chat";
+const description = "The best chat app equiped with privacy";
+const url = "https://jjchat-six.vercel.app";
+const image = "https://firebasestorage.googleapis.com/v0/b/chat-jj.appspot.com/o/20230708_084740.png?alt=media&token=885103b6-6254-43d9-bfe8-c28018679e45";
 export const metadata = {
-    title: "JJ Chat",
-    description: "The best chat app equiped with privacy"
+    title: title,
+    description: description,
+    openGraph: {
+      title: title,
+      description: description,
+      url: url,
+      images: image,
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: title,
+      description: description,
+      images: [image],
+    },
 }
 
 export default function RootLayout({
